e99852b8b65b4071bf0212a3e006e1e7735cd312
sWitched-0ff <azhuravlov@gmail.com>
2013-05-23 10:33:31 +0400

    new size 70%

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-	$('#seasoncalendar a').hover(function(){
-		$('#'+this.id+'img').fadeIn();
-		$('.backgroundLine div:not(#'+this.id+'img)').fadeOut('slow');
+	//$('#seasoncalendar a').hover(function(){
+	//	$('#'+this.id+'img').fadeIn();
+	//	$('.backgroundLine div:not(#'+this.id+'img)').fadeOut('slow');
+	//});
+
+	var docWidth=$('body').width();
+	$('#mycarousel').width(docWidth);
+	$('#mycarousel li').width(docWidth);
+
+	$('#mycarousel').jcarousel({
+		scroll:1,
+		auto:5,
+		wrap: 'circular',
+		initCallback: mycarousel_initCallback,
+		buttonNextHTML: null,
+		buttonPrevHTML: null
 	});
 });
+
+function mycarousel_initCallback(carousel) {
+	$('#seasoncalendar a').bind('hover', function() {
+		var index = $(this).index();
+		carousel.scroll($.jcarousel.intval(index));
+	});
+  };
